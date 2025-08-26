3c1f2679642e986c7605ec700759a9b8cc873467
Nuraniya Suleimanova <ishmuratova2013@yandex.ru>
2025-08-26 10:59:36 +0100

    Добавлен полный проект: автотесты для Stellar Burgers

---
+++ urls.py
@@ -0,0 +1,7 @@
+class Urls:
+    # Основные URL-адреса приложения Stellar Burgers
+    MAIN_PAGE = "https://stellarburgers.nomoreparties.site/"
+    LOGIN_PAGE = "https://stellarburgers.nomoreparties.site/login"
+    REGISTER_PAGE = "https://stellarburgers.nomoreparties.site/register"
+    FORGOT_PASSWORD_PAGE = "https://stellarburgers.nomoreparties.site/forgot-password"
+    PROFILE_PAGE = "https://stellarburgers.nomoreparties.site/account/profile"
